47f8021f07098860f6983729142dd3842833f922
Arthur Hulsman <hulsman1986@gmail.com>
2017-12-18 15:20:09 +0100

    VGD-551 - Added deploy Grunt task.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,10 @@
+function atob(str) {
+    if (str) {
+        return new Buffer(str, 'base64').toString('binary');
+    }
+    return null;
+}
+
 module.exports = function(grunt) {
 
     const startTS = Date.now();
@@ -102,6 +109,7 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-banner');
+    grunt.loadNpmTasks('grunt-google-cloud');
 
     // Register tasks.
     grunt.registerTask('duration',
@@ -148,4 +156,76 @@ module.exports = function(grunt) {
         ];
         grunt.task.run(tasksArray);
     });
+    grunt.registerTask('deploy',
+        'Upload the build files.',
+        function() {
+            const project = grunt.option('project'), // vooxe-gamedistribution
+                bucket = grunt.option('bucket'), // gd-sdk-html5
+                folderIn = grunt.option('in'), //
+                folderOut = grunt.option('out'); //
+
+            // The key is saved as a system parameter within Team City.
+            // The service account key of our google cloud account for uploading to
+            // storage is stringified and then encoded as base64 using btoa()
+            console.log(grunt.option('key'));
+            let keyObj = grunt.option('key');
+            let key = JSON.parse(atob(keyObj));
+            console.log(key);
+
+            if (project === undefined) {
+                grunt.fail.warn('Cannot upload without a project name');
+            }
+
+            if (bucket === undefined) {
+                grunt.fail.warn('OW DEAR GOD THEY ARE STEALING MAH BUCKET!');
+            }
+
+            if (key === undefined || key === null) {
+                grunt.fail.warn('Cannot upload without an auth key');
+            } else {
+                console.log('Key loaded...');
+            }
+
+            grunt.config.merge({
+                gcs: {
+                    options: {
+                        credentials: key,
+                        project: project,
+                        bucket: bucket,
+                        gzip: true,
+                        metadata: {
+                            'surrogate-key': 'gcs',
+                        },
+                    },
+                    dist: {
+                        cwd: './lib/',
+                        src: ['**/*'],
+                        dest: '',
+                    },
+                },
+            });
+
+            console.log('Project: ' + project);
+            console.log('Bucket: ' + bucket);
+
+            if (folderIn === undefined && folderOut === undefined) {
+                console.log('Deploying: ./lib/ to gs://' + bucket + '/');
+            } else {
+                if (folderIn !== undefined) {
+                    if (folderOut === undefined) {
+                        grunt.fail.warn(
+                            'No use in specifying "in" without "out"');
+                    }
+                    console.log('Deploying: ../' + folderIn + ' to gs://' +
+                        bucket + '/' + folderOut);
+                    grunt.config.set('gcs.dist', {
+                        cwd: '../' + folderIn, src: ['**/*'], dest: folderOut,
+                    });
+                } else if (folderOut !== undefined) {
+                    grunt.fail.warn('No use in specifying "out" without "in"');
+                }
+            }
+
+            grunt.task.run('gcs');
+        });
 };
